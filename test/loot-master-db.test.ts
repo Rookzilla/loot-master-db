3d65645c76f17de401878859b71471c4f49f0f72
Andy Brown <andybrown7890@gmail.com>
2024-06-20 12:57:15 +0100

    Initial commit

---
+++ test/loot-master-db.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as LootMasterDb from '../lib/loot-master-db-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/loot-master-db-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new LootMasterDb.LootMasterDbStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
